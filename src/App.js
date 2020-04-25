09ba62af3137fb7465371c44d345f15683a59267
Leticia G <leticiah.helena@hotmail.com>
2020-04-24 23:54:36 -0300

    integração com backend

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from "react";
+import React ,{useState,useEffect}from "react";
 
 import "./styles.css";
 
+import api from './services/api';
+
 function App() {
+
+  const [repositories,setRepositories] = useState([]);
+
+ 
+  useEffect(() => {
+     api.get('repositories').then(response => {setRepositories(response.data)});
+  });
+
+
   async function handleAddRepository() {
-    // TODO
+   const response = await api.post('repositories',{title:'Repository',url:'http://example.com', techs:['javascript','node.js']});
+
+    const repository = response.data;
+    
+    setRepositories([...repositories,repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+   await api.delete(`repositories/${id}`);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+      {repositories.map(repos =>
+        <li key={repos.id}>
+          
+          {repos.title} 
+          <button onClick={() => handleRemoveRepository(repos.id)}>
             Remover
           </button>
-        </li>
+        </li>)}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
